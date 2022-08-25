ae88c6fe0e969a07049bbb2cb68a31ca8b215490
mannchaft <2kick2014@gmail.com>
2022-08-25 23:39:03 +0100

    1st Version

---
+++ src/components/Edition.vue
@@ -0,0 +1,150 @@
+<template>
+    <v-form v-model="valid"> 
+        <!-- ToolBox for activities -->
+    <v-container  fluid>
+        <v-row class="toolbox text-uppercase align-center">
+            <v-col cols="4" md="2">
+                <h4 class="text-center blublu--text">Structured activities</h4>
+                <div class="d-flex justify-center align-center">
+                <v-btn v-ripple="{ class: 'primary--text' }" text color="#90CAF9" right>
+                <v-icon>mdi-trending-neutral</v-icon>
+                </v-btn> 
+                <v-btn v-ripple="{ class: 'primary--text' }" text color="#90CAF9" right>
+                <v-icon>mdi-shuffle-disabled</v-icon>
+                </v-btn>
+                </div>
+            </v-col>
+        <v-divider vertical></v-divider>
+            <v-col cols="4" md="2">
+                <h4 class="text-center blublu--text">Loop activities</h4>
+                <div class="d-flex justify-center align-center">
+                <v-btn small v-ripple="{ class: 'primary--text' }" text color="#90CAF9">
+                <v-icon>mdi-repeat</v-icon>
+                <!-- at click -> popup to enter a condition -->
+                </v-btn>
+                <v-btn small v-ripple="{ class: 'primary--text' }" text color="#90CAF9">
+                <v-icon>mdi-sync</v-icon>
+                <!-- at click -> popup to enter a condition -->
+                </v-btn>
+                <v-btn small v-ripple="{ class: 'primary--text' }" text color="#90CAF9">
+                <v-icon>mdi-reload</v-icon>
+                <!-- at click -> popup to enter a condition -->
+                </v-btn>
+                </div>
+            </v-col>
+        <v-divider vertical></v-divider>
+            <v-col cols="4" md="2">
+                <h4 class="text-center blublu--text">Condition activity</h4>
+                <div class="d-flex justify-center align-center">
+                <v-btn small v-ripple="{ class: 'primary--text' }" text color="#90CAF9">
+                <v-icon>mdi-alert-circle-check-outline</v-icon>
+                <!-- at click -> popup to enter a condition -->
+                </v-btn>
+                </div>
+            </v-col>
+        <v-divider vertical></v-divider>
+            <v-col cols="4" md="2">
+                <h4 class="text-center blublu--text"> Communication activities </h4>
+                <div class="d-flex justify-center align-center">
+                <v-btn small v-ripple="{ class: 'primary--text' }" text color="#90CAF9">
+                <v-icon>mdi-web-remove</v-icon></v-btn>
+
+                <v-btn small v-ripple="{ class: 'primary--text' }" text color="#90CAF9">
+                <v-icon>mdi-web-plus</v-icon></v-btn>
+
+                <v-btn small v-ripple="{ class: 'primary--text' }" text color="#90CAF9">
+                <v-icon>mdi-web-sync</v-icon></v-btn>
+
+                <v-btn small v-ripple="{ class: 'primary--text' }" text color="#90CAF9">
+                <v-icon>mdi-search-web</v-icon></v-btn>
+                
+                </div>
+            </v-col>
+        <v-divider vertical></v-divider>
+        <v-col>
+            <div class="d-flex justify-center align-center">
+            <v-select label="Choose an object" color="#90CAF9" dark></v-select>
+            <v-icon dark color="#90CAF9">mdi-web</v-icon>
+            </div>
+        </v-col>
+        </v-row>
+
+    <!-- temporal constraints -->
+        <v-btn fab color="#dd52a3" dark bottom left absolute @click="dialog = !dialog">
+            <v-icon>mdi-plus</v-icon>
+        </v-btn>
+                 
+        <v-dialog v-model="dialog" max-width="500px">
+            <v-card>
+                <v-card-text>
+                    <!-- Contraintes qui peuvent etre saisi ou rempli aleatoirement -->
+                    <h1 class="pa-4">Temporal values :</h1>
+                    <v-text-field color="#673AB7" label="DD" placeholder="Must start at ... " class="mt-8" outlined dense></v-text-field>
+                    <v-text-field color="#673AB7" label="DF" placeholder="Must end at ... " class="mt-8" outlined dense></v-text-field>
+                    <v-text-field color="#673AB7" type="number" class="inputPrice" label="Year"></v-text-field>
+                    <v-text-field color="#673AB7" type="number" class="inputPrice" label="Month"></v-text-field>
+                    <v-text-field color="#673AB7" type="number" class="inputPrice" label="Day"></v-text-field>
+                    <v-text-field color="#673AB7" type="number" class="inputPrice" label="Hours"></v-text-field>
+                    <v-text-field color="#673AB7" type="number" class="inputPrice" label="Minutes"></v-text-field>
+                    <v-text-field color="#673AB7" type="number" class="inputPrice" label="Seconds"></v-text-field>
+                    <!-- Contraintes obligatoires -->
+                    <h1 class="pa-4">Temporal constraints :</h1>
+                    <v-text-field color="#673AB7" label="CD" placeholder="Must start at ... " class="mt-8" outlined dense></v-text-field>
+                    <v-text-field color="#673AB7" label="CF" placeholder="Must end at ... " class="mt-8" outlined dense></v-text-field>
+                    <v-select :items="TC"  label="TC" outlined class="mt-8" color="#673AB7"></v-select>
+                </v-card-text>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="primary" class="mb-4" @click="dialog = false">Submit</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+        </v-container>
+  </v-form>
+</template>
+<script>
+export default ({
+    data() {
+    return {
+        TC: [{text: 'Before'}, {text: 'Between'}, {text: 'After'}],
+        dialog: false,
+        Objects: [{text: 'Before'}, {text: 'Between'}, {text: 'After'}]
+    }
+   }
+})
+</script>
+
+<style scoped>
+.toolbox {
+    background-color: #673AB7 ;
+}
+.objects {
+    background-color: #C5CAE9;
+    height: 800px;
+}
+/* setting min-width is necessary for the width to work */
+.v-btn {
+    min-width: 0;
+}
+/* removes grey hover effect */
+.v-btn::before{
+  background-color: transparent;
+}
+
+/* adds icon scale hover effect */
+.v-btn i:hover{
+  transform: scale(1.15);
+}
+ .v-text-field .v-input__control .v-input__slot {
+    min-height: auto !important;
+    display: flex !important;
+    align-items: center !important;
+  }
+.inputPrice input[type='number'] {
+    -moz-appearance:textfield;
+}
+.inputPrice input::-webkit-outer-spin-button,
+.inputPrice input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+}
+</style>
---
+++ src/views/LoginAndSignUp.vue
@@ -0,0 +1,141 @@
+<template>
+  <v-container>
+    <v-row align="center" justify="center">
+      <v-col cols="12" sm="10">
+       <v-card class="elevation-6 mt-10">
+        <v-window v-model="step">
+          <v-window-item :value="1">
+            <v-row>
+              <v-col cols="12" sm="6">
+                <v-card-text class="mt-12">
+                  <h1 class="text-uppercase black--text text-center">
+                <span class="font-weight-light">Temporal</span>
+                <span>Scenario editor</span>
+                  </h1>
+                  <h4 class="text-center grey--text">Edit, create, organize and control your surrounding </h4>
+                  <v-row align="center" justify="center">
+                    <v-col cols="12" sm="8">
+                      <v-text-field label="Email" outlined dense color="indigo" autocomplete="false" class="mt-16"></v-text-field>
+                      <v-text-field label="Password" outlined dense color="indigo" autocomplete="false" type="password"></v-text-field>
+                      <v-row>
+                        <v-col cols="12" sm="7">
+                          <v-checkbox label="Remember me" class="mt-n1" color="pink"></v-checkbox>
+                        </v-col>
+                        <v-col cols="12" sm="5">
+                          <span class="caption blue--text">Forgot password</span>
+                        </v-col>
+                      </v-row>
+                     <v-btn :to="{name: 'home'}" color="indigo" dark block tile>Login</v-btn>
+                      <h4 class="text-center grey--text mt-4 mb-3">Or</h4>
+                      <div class="d-flex jjustify-space-between align-center mx-10 mb-16">
+                        <v-btn depressed outlined color="grey">
+                          <v-icon color="red">fab fa-google</v-icon>
+                        </v-btn>
+                        <v-btn depressed outlined color="grey">
+                          <v-icon color="blue">fab fa-facebook-f</v-icon>
+                        </v-btn>
+                        <v-btn depressed outlined color="grey">
+                          <v-icon color="light-blue lighten-3">fab fa-twitter</v-icon>
+                        </v-btn>
+                      </div>
+                    </v-col>
+                  </v-row>
+                </v-card-text>
+              </v-col>
+              <v-col cols="12" sm="6" class="hero blue rounded-bl-xl">
+                <div style="text-align:center; padding: 45px 0;">
+                  <v-card-text class="white--text">
+                    <h1 class="text-center">First time using our editor ?</h1>
+                    <h4 class="text-center">
+                      <br> Let's get you all set up so you can start creating your first <br> IoT scenario ! Click on the button just below<br>
+                    </h4>
+                  </v-card-text>
+                  <div class="text-center" style="padding: 185px 0;">
+                    <v-btn tile outlined dark @click="step++">SIGN UP HERE</v-btn>
+                  </div>
+                </div>
+              </v-col>
+            </v-row>
+          </v-window-item>
+          <v-window-item :value="2">
+            <v-row>
+              <v-col cols="12" sm="6" class="hero blue rounded-br-xl">
+                <div style="text-align: center; padding:45px 0">
+                  <v-card-text class="white--text">
+                    <h1 class="text-center">Oh ! Already signed up ?</h1>
+                    <h4 class="text-center"><br>You can go back to login thanks to the button just below </h4>
+                  </v-card-text>
+                  <div class="text-center" style="padding: 185px 0;">
+                    <v-btn tile outlined dark @click="step--">GO BACK</v-btn>
+                  </div>
+                </div>
+              </v-col>
+              <v-col cols="12" sm="6">
+                <v-card-text class="mt-12">
+                  <h1 class="text-uppercase black--text text-center">Sign up</h1>
+                  <h4 class="text-center grey--text">
+                   Let's get started !
+                  </h4>
+                  <v-row align="center" justify="center">
+                    <v-col cols="12" sm="8">
+                      <v-row>
+                        <v-col cols="12" sm="6">
+                          <v-text-field label="First Name" outlined dense color="blue" autocomplete="false" class="mt-4"></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6">
+                          <v-text-field label="Last Name" outlined dense color="blue" autocomplete="false" class="mt-4"></v-text-field>
+                        </v-col>
+                      </v-row>
+                      <v-text-field label="Email" outlined dense color="blue" autocomplete="false"></v-text-field>
+                      <v-text-field label="Password" outlined dense color="blue" type="password"></v-text-field>
+                      <v-row>
+                        <v-col cols="12" sm="7">
+                          <span class="caption blue--text ml-n4">Terms & Conditions</span>
+                        </v-col>
+                        <v-col cols="12" sm="5">
+                          <v-checkbox label="I accept" class="mt-n1" color="blue"></v-checkbox>
+                        </v-col>
+                      </v-row>
+                      <v-btn color="indigo" dark block tile>Sign up</v-btn>
+                    </v-col>
+                  </v-row>
+                </v-card-text>
+              </v-col>
+            </v-row>
+          </v-window-item>
+        </v-window>
+       </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+
+  export default {
+  data: () => ({
+    step: 1
+  }),
+  props: {
+    source: String
+  }
+  }
+</script>
+<style scoped>
+.v-container {
+     background: url('../../public/vecteezy_neon-background-1_dy1220.jpg')
+    no-repeat center center fixed !important;
+    background-size: cover;
+}
+.v-application .rounded-bl-xl {
+  border-bottom-left-radius: 300px !important;
+}
+.v-application .rounded-br-xl {
+  border-bottom-right-radius: 300px !important;
+}
+.hero {
+  background: url('../../public/vecteezy_neon-background-1_dy1220.jpg');
+  background-size: cover;
+  height: 100vh;
+}
+</style>
